45c72f6af371b2e2064ed81c28ef099c9409d178
Fellip15 <fellipalves15@gmail.com>
2022-07-22 13:54:44 -0300

    Comentando o código

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,4 +1,8 @@
 class Forca {
+  /**
+   * 
+   * @param {string} gabarito palavra que será utilizada na forca
+   */
   constructor(gabarito) {
     this.gabarito = gabarito                          // Palavra original
     this.palavra = Array(gabarito.length).fill("_");  // Palavra com máscara: "_"
@@ -9,22 +13,32 @@ class Forca {
     this.estadoAtual = "aguardando chute";
   }
 
-  erroLetra(letra) {
-    // Se ela não é alfabética retorne true, caso contrário false
-    return !(/^[a-z]/i.test(letra));
+  /**
+   * 
+   * @param {string} letra letra para ser verificada
+   * @returns {boolean} true se ela é uma letra alfabética, false caso contrário
+   */
+  ehAlfabetica(letra) {
+    // Se ela é alfabética retorne true, caso contrário false
+    return (/^[a-z]/i.test(letra));
   }
 
+  /**
+   * 
+   * @param {string} letra chute do usuário
+   * @returns {boolean} true se o chute for válido, false caso contrário
+   */
   chutar(letra) {
     letra = letra.toLowerCase();
     // Verifica a quantidade de letras, se possui erro na letra e se ela já foi chutada
-    if (letra.length != 1 || this.erroLetra(letra) || this.letrasChutadas.includes(letra)) 
-      return;
+    if (letra.length != 1 || !this.ehAlfabetica(letra) || this.letrasChutadas.includes(letra)) 
+      return false;
     this.letrasChutadas.push(letra);
 
     // Se ela não existe no gabarito
     if (!this.gabarito.includes(letra)) {
       this.vidas -= 1;
-      return;
+      return false;
     }
 
     // Adiciona ela na this.palavra nos lugares em que ela existe no gabarito
@@ -32,8 +46,15 @@ class Forca {
       if (this.gabarito[i] === letra) 
         this.palavra[i] = letra;
     }
+
+    return true;
   }
 
+  /**
+   * 
+   * @returns {string} "aguardando chute", "ganhou" ou "perdeu",
+   * estado atual do jogo
+   */
   buscarEstado() {
     if (this.vidas <= 0) this.estadoAtual = "perdeu";
     else if (!this.palavra.includes("_")) this.estadoAtual = "ganhou";
@@ -42,6 +63,10 @@ class Forca {
     return this.estadoAtual; 
   } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
+  /**
+   * 
+   * @returns {object} letras chutadas, vidas e a palavra atual
+   */
   buscarDadosDoJogo() {
       return {
           letrasChutadas: this.letrasChutadas, // Deve conter todas as letras chutadas
